58c384d9849c58ce6fcbb8603fcfc2bd6ae3d0e7
Audrey POLVERINI <audrey.polverini@gmail.com>
2024-10-18 17:54:34 +0200

    Installation react router, organisation project and files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    // Vos règles personnalisées ici
+  },
+};
