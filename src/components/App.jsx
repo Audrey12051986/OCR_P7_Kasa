e9d5b827ce09434741268ee137f5d6c643b317f0
Audrey POLVERINI <audrey.polverini@gmail.com>
2024-10-30 19:19:04 +0100

    Built error page: code and style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home"; // Chemin correct vers HomePage
 import AboutPage from "../pages/AboutPage"; // Chemin correct vers AboutPage
+import ErrorPage from "../pages/ErrorPage";
 import Header from "./Header"; // Chemin correct vers Header
 import Footer from "./Footer";
 import "../styles/index.scss";
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "_variables"; // où variables.scss contient la définition de $primary-color
 
 function Error() {
   return (
     <section className="error">
       <h1 className="error__title">404</h1>
-      <h3 className="error__subtitle">
+      <h2 className="error__subtitle">
         Oups! La page que vous demandez n&apos;existe pas.
-      </h3>
+      </h2>
       {/* Lien vers la page d'accueil */}
       <Link className="error__link" to="/">
         Retourner sur la page d&apos;accueil
