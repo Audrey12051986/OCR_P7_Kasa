9debcf58f64ef667f97cdefb6bffd2b31496d1a1
Audrey POLVERINI <audrey.polverini@gmail.com>
2024-11-08 15:49:37 +0100

    update property page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home"; // Chemin correct vers HomePage
 import AboutPage from "../pages/AboutPage"; // Chemin correct vers AboutPage
 import ErrorPage from "../pages/ErrorPage";
+import PropertyPage from "../pages/PropertyPage";
 import Header from "./Header"; // Chemin correct vers Header
 import Footer from "./Footer";
 import "../styles/index.scss";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/error" element={<ErrorPage />} />
+        <Route path="/property" element={<PropertyPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
